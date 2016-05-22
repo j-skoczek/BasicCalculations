336dd65a469a99e1967bf4e2f0257aacabea2a29
j-skoczek <jendolf@gmail.com>
2016-05-22 18:20:15 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -14,15 +14,3 @@ class Main {
 		System.out.println(r.getPeremiter(p1, p2, p3));
 	}
 }
-
-//@Test
-//public void testGetArea() {
-//	Point p1 = new Point(0, 0);
-//	Point p2 = new Point(2, 0);
-//	Point p3 = new Point(0, 3);
-//	Point p4 = new Point(2, 3);
-//
-//	Rectangle r = new Rectangle(p1, p2, p3, p4);
-//
-//	assertEquals(6, r.getArea());
-//}
